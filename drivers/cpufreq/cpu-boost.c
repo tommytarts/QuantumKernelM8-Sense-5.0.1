73c03982e64faf20e70a1c722728e79f8a727201
Tom Tartaro <tomtartaro@gmail.com>
2015-02-12 20:15:00 -0800

    Revert "cpufreq: cpu-boost: Introduce scheduler assisted load based syncs"
    This reverts commit 261b773367a11eeb9b0cd2045943fc7ae4497b10.
    
    Conflicts:
    	drivers/cpufreq/cpu-boost.c

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -37,7 +37,6 @@ struct cpu_sync {
 	int src_cpu;
 	unsigned int boost_min;
 	unsigned int input_boost_min;
-	unsigned int task_load;
 	unsigned int input_boost_freq;
 };
 
@@ -61,12 +60,6 @@ static bool input_boost_enabled;
 
 static unsigned int input_boost_ms = 40;
 module_param(input_boost_ms, uint, 0644);
-
-static unsigned int migration_load_threshold = 30;
-module_param(migration_load_threshold, uint, 0644);
-
-static bool load_based_syncs = 1;
-module_param(load_based_syncs, bool, 0644);
 
 static bool hotplug_boost = 1;
 module_param(hotplug_boost, bool, 0644);
@@ -244,7 +237,6 @@ static void run_boost_migration(unsigned int cpu)
 	struct cpufreq_policy dest_policy;
 	struct cpufreq_policy src_policy;
 	unsigned long flags;
-	unsigned int req_freq;
 
 	spin_lock_irqsave(&s->lock, flags);
 	s->pending = false;
@@ -259,21 +251,17 @@ static void run_boost_migration(unsigned int cpu)
 	if (ret)
 		return;
 
-	req_freq = load_based_syncs ?
-		(dest_policy.max * s->task_load) / 100 :
-						src_policy.cur;
-
-	if (req_freq <= dest_policy.cpuinfo.min_freq) {
-			pr_debug("No sync. Sync Freq:%u\n", req_freq);
+	if (src_policy.min == src_policy.cpuinfo.min_freq) {
+		pr_debug("No sync. Source CPU%d@%dKHz at min freq\n",
+				src_cpu, src_policy.cur);
 		return;
 	}
 
-	if (sync_threshold)
-		req_freq = min(sync_threshold, req_freq);
-
 	cancel_delayed_work_sync(&s->boost_rem);
-
-	s->boost_min = req_freq;
+	if (sync_threshold)
+		s->boost_min = min(sync_threshold, src_policy.cur);
+	else
+		s->boost_min = src_policy.cur;
 
 	/* Force policy re-evaluation to trigger adjust notifier. */
 	get_online_cpus();
@@ -325,19 +313,10 @@ static struct smp_hotplug_thread cpuboost_threads = {
 };
 
 static int boost_migration_notify(struct notifier_block *nb,
-				unsigned long unused, void *arg)
+				unsigned long dest_cpu, void *arg)
 {
-	struct migration_notify_data *mnd = arg;
 	unsigned long flags;
-	struct cpu_sync *s = &per_cpu(sync_info, mnd->dest_cpu);
-
-	if (load_based_syncs && (mnd->load <= migration_load_threshold))
-		return NOTIFY_OK;
-
-	if (load_based_syncs && ((mnd->load < 0) || (mnd->load > 100))) {
-		pr_err("cpu-boost:Invalid load: %d\n", mnd->load);
-		return NOTIFY_OK;
-	}
+	struct cpu_sync *s = &per_cpu(sync_info, dest_cpu);
 
 	if (!load_based_syncs && (mnd->src_cpu == mnd->dest_cpu))
 		return NOTIFY_OK;
@@ -349,11 +328,10 @@ static int boost_migration_notify(struct notifier_block *nb,
 	if (thread == current)
 		return NOTIFY_OK;
 
-	pr_debug("Migration: CPU%d --> CPU%d\n", mnd->src_cpu, mnd->dest_cpu);
+	pr_debug("Migration: CPU%d --> CPU%d\n", (int) arg, (int) dest_cpu);
 	spin_lock_irqsave(&s->lock, flags);
 	s->pending = true;
-	s->src_cpu = mnd->src_cpu;
-	s->task_load = load_based_syncs ? mnd->load : 0;
+	s->src_cpu = (int) arg;
 	spin_unlock_irqrestore(&s->lock, flags);
 
 	return NOTIFY_OK;
